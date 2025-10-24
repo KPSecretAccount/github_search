429796ae4a025dbd101f72f7ca56f006da9084a9
KPSecretAccount <sampleemail4github@gmail.com>
2025-10-24 11:26:58 -0400

    :)

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import {useEffect, useState} from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+export default function App() {
+const[users, setUsers] = useState<{login:string, avatar_url:string}[]>([]);
+const[result,search] = useState<string>() ;
+useEffect(() => {
+    fetch("https://api.github.com/search/users?q="+ result +"+in:login")
+        .then((response) => response.json())
+        .then((data) => {
+            setUsers(data.items);
+            console.log(data);
+        })
+        .catch((error) => console.log(error));
+}, [result]);
+return (
+    <>
+    <label>
+        <input id = "myInput" placeholder={"Type to search"}/>
+    </label>
+    <button onClick={() => {
+        const newVal = (document.getElementById('myInput') as HTMLInputElement).value;
+        search(newVal);
+    }}>search</button>
+        {
+            users.map((user, index) =>
+                <div key = {index}>
+                    <img src ={user.avatar_url}
+                     alt = {user.login}/> {user.login}
+                </div>
+            )
+        }
+</>)
+    }
